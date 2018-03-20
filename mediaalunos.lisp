3e0140e2eb3767084f29fd97d813d2a08f69edf0
AlbericoD <a.diasbarreto@icloud.com>
2018-03-19 23:06:50 -0300

    Arquivos iniciais

---
+++ mediaalunos.lisp
@@ -0,0 +1,21 @@
+;;;;; Media alunos
+    ;;; Requisitos
+        ;;; N1 (50%)
+            ;; 1 Prova (30%),
+            ;; 4 projetos (30%),
+            ;; 1 artigo (30%) e
+            ;; 1 projeto integrador (10%)
+        ;;; N2 (30%)
+            ;; 1 Prova
+        ;;; N3 (20%)
+            ;; 1 Prova
+
+(format t "Media Alunos~%~%")
+
+(defun n1 (nota_prova n2 n3 n4)
+   (/ ( + n1 n2 n3 n4) 4)
+)
+(defun calc_projetos(p1 p2 p3 p4)
+
+)
+    ;;(write(averagenum 10 20 30 40))
