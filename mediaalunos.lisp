41580c39039ae852b547f1a61297aee260011d1b
AlbericoD <a.diasbarreto@icloud.com>
2018-03-24 12:08:32 -0300

    Adcionado prova 0.3%

--- mediaalunos.lisp
+++ mediaalunos.lisp
@@ -12,13 +12,14 @@
 
 (format t "Media Alunos~%~%")
 
-    (defun calcProjetos(proje1 proje2 proje3 proje4 artigo projeIntegrador) "Projetos(4-30%),Artigo(30%),Integrador(10%)."
+    (defun calcProjetos(proje1 proje2 proje3 proje4 artigo projeIntegrador prova) "Projetos(4-30%),Artigo(30%),Integrador(10%), Prova(30%)."
         (+ (* (/ (+ proje1 proje2 proje3 proje4) 4) 0.3)
-        (* artigo 0.3)(* projeIntegrador 0.1))
-    )   ;; Teste com valores falsos 
-        (write(calcProjetos 25 25 25 25 25 25))
+        (* artigo 0.3)(* projeIntegrador 0.1)(* prova 0.3))
+    )   ;; Teste com valores falsos resultado tem que ser 6.16.1
+        (write(calcProjetos 2 2 2 2 5 10 10))
 
     (defun calcMedia(notaN1 notaN2 notaN3) "N1(50%)|N2(30%)|N3(20%)"
         (+ (* notaN1 0.5) (* notaN2 0.3) (* notaN3 0.2))        
-    )   ;; Teste com valores falsos resultado tem que ser 7,1
+    )   ;; Teste com valores falsos resultado tem que ser 7.71.1
         (write(calcMedia 10 5 3))    
+
